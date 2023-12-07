9665186c691b9a34f1c6448bea4166e1cac189d2
Braeden3 <149532454+Braeden3@users.noreply.github.com>
2023-12-07 13:06:50 -0800

    Add files via upload

--- DrivingV2.c
+++ DrivingV2.c
@@ -12,15 +12,34 @@
 #include <driverlib/gpio.h>
 #include <driverlib/adc.h>
 #include <driverlib/pwm.h>
+#include <driverlib/uart.h>
 #include <WheelsV2.h>
+#include <radioV2.h>
 
-int32_t pwmLeft, pwmRight, absLeft, absRight, directionLeft, directionRight, angleScaled;
+int32_t pwmLeft, pwmRight, absLeft, absRight, directionLeft, directionRight;
 int32_t maxSpeed = 10000;
 int32_t DZ = 100;
+int16_t receivedS;
+int8_t received;
 
-int DrivingV2(left, right, angle, function){
+int DrivingV2(left, right, function){
+    static int8_t angle;
+
+    //Check if alternate function is enabled, alternate function enables LIDAR control
     if ( function == true ){
         absLeft = abs( left - DZ );
+
+        //Get data from UART
+        while(UARTCharsAvail(UART0_BASE)){
+            UARTRxErrorClear(UART0_BASE);
+            int8_t received = UARTCharGet(UART0_BASE);
+            if (UARTRxErrorGet(UART0_BASE)) continue;
+            angle = received;
+            receivedS += angle;
+            angle = receivedS / 2;
+        }
+        printf("angle %d", angle);
+        //If left stick is in the dead zone set all values for WheelsV2 to 0
         if ( absLeft <= 10 ){
             pwmLeft = 0;
             pwmRight = 0;
@@ -28,62 +47,77 @@ int DrivingV2(left, right, angle, function){
             directionLeft = 0<<4;
         }
         else{
-            if ( angle < 90 ){
-                pwmLeft = maxSpeed + 100 * ( 100 - absLeft + angle );
+
+            //Will turn in place toward the left at a speed determined by the controllers left stick
+            if ( angle <= -120 ){
+                pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
                 pwmRight = maxSpeed + 100 * ( 100 - absLeft );
-                directionRight = ( left > DZ ) ? 0<<5 : 1<<5;
-                directionLeft = 1<<4;
-            }
-            else if ( angle < 180 ){
-                angleScaled = 180 - angle;
-                pwmLeft = maxSpeed + 100 * ( 100 - absLeft + angleScaled );
-                pwmRight = maxSpeed + 100 * ( 100 - absLeft );
-                directionRight = ( left > DZ ) ? 0<<5 : 1<<5;
+                directionRight = 0<<5;
                 directionLeft = 0<<4;
             }
-            else if ( angle < 270 ){
-                angleScaled = angle - 180;
-                pwmRight = maxSpeed + 100 * ( 100 - absLeft + angleScaled );
+
+            //Will turn in place toward the right at a speed determined by the controllers left stick
+            else if ( angle >= 120 ){
                 pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
-                directionRight = 0<<5;
-                directionLeft = ( left > DZ ) ? 1<<4 : 0<<4;
-            }
-            else{
-                angleScaled = 360 - angle;
-                pwmRight = maxSpeed + 100 * ( 100 - absLeft + angleScaled );
-                pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
+                pwmRight = maxSpeed + 100 * ( 100 - absLeft );
                 directionRight = 1<<5;
-                directionLeft = ( left > DZ ) ? 1<<4 : 0<<4;
+                directionLeft = 1<<4;
+            }
+
+            //If angle received is less than -9 reduce the speed of the left side wheels
+            else if ( angle < -9 ){
+                pwmLeft = maxSpeed + 100 * ( 100 - absLeft + abs(angle) );
+                pwmRight = maxSpeed + 100 * ( 100 - absLeft );
+                directionRight = 0<<5;
+                directionLeft = 1<<4;
+            }
+
+            //If angle received is greater than 9 reduce the speed of the right side wheels
+            else if ( angle > 9 ){
+                pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
+                pwmRight = maxSpeed + 100 * ( 100 - absLeft + abs(angle) );
+                directionRight = 0<<5;
+                directionLeft = 1<<4;
+            }
+
+            //Else left and right side will have the same speed
+            else{
+                pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
+                pwmRight = maxSpeed + 100 * ( 100 - absLeft );
+                directionRight = 0<<5;
+                directionLeft = 1<<4;
             }
         }
     }
 
     else {
+        //Normal function is tank control via controller
+
         //left and right will go from 0 - 200
         //DZ will equal 100 and the dead zone being 100 +/- 5
         absLeft = abs( left - DZ );
-        absRight = abs (right - DZ );
+        absRight = abs( right - DZ );
 
 
-        //calculate clock cycles for PWM
+        //calculate clock cycles for PWM period
         if ( absLeft <= 10 ){
             pwmLeft = 0;
         } else{
-            pwmLeft = maxSpeed + 100 * (100 - absLeft);
+            pwmLeft = maxSpeed + 100 * ( 100 - absLeft );
         }
         if ( absRight <= 10 ){
             pwmRight = 0;
         } else{
-            pwmRight = maxSpeed + 100 * (100 - absRight);
+            pwmRight = maxSpeed + 100 * ( 100 - absRight );
         }
 
         //determine rotation of stepper motors
         directionRight = ( right > DZ ) ? 0<<5 : 1<<5;
         directionLeft = ( left > DZ ) ? 1<<4 : 0<<4;
     }
-    /*
-     * Call Wheels.c and provide necessary values.
-     */
+    printf(" left speed %d right speed %d left dir %d right dir %d alt0 %d\n",
+           pwmLeft, pwmRight, directionLeft, directionRight, alternate_functions[0]);
+    //Call WheelsV2 and provide necessary values
     WheelsV2(pwmRight, pwmLeft, directionRight, directionLeft);
     return 0;
 }
